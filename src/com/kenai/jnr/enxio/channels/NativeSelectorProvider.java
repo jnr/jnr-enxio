8466786cc8f38259a8a19b25bcde6cc89b914297
Wayne Meissner <wmeissner@gmail.com>
2009-11-09 17:51:34 +1000

    Add LGPLv3 copyright

--- src/com/kenai/jnr/enxio/channels/NativeSelectorProvider.java
+++ src/com/kenai/jnr/enxio/channels/NativeSelectorProvider.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the JNR project.
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * version 3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.kenai.jnr.enxio.channels;
@@ -16,10 +27,7 @@ import java.nio.channels.spi.SelectorProvider;
 import com.kenai.jnr.enxio.channels.kqueue.KQSelector;
 import com.kenai.jnr.enxio.channels.poll.PollSelector;
 
-/**
- *
- * @author wayne
- */
+
 public final class NativeSelectorProvider extends SelectorProvider {
     private static final class SingletonHolder {
         static NativeSelectorProvider INSTANCE = new NativeSelectorProvider();
